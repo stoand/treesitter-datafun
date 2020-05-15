b9d44c8b80fd742ad5bd0699df95e0ffdbc07cc1
Andreas Stocker <andreas@stockers.org>
2020-05-15 04:28:15 +0300

    basic vars work

--- grammar.js
+++ grammar.js
@@ -2,16 +2,38 @@
 module.exports = grammar({
     name: 'datafun',
     rules: {
-        source_file: $ => repeat($.block),
-        block: $ => seq($.term, repeat(';')),
-        term: $ => choice(
-            'bool',
-            'int',
+        source_file: $ => repeat1($.block),
+
+        block: $ => seq($.expr, repeat(';')),
+        
+        expr: $ => choice(
+            $.var_name,
+            $.comprehension,
         ),
+        var_name: $ => token(/[A-Za-z-_][A-Za-z-_0-9]*/),
+        _ws: $ => repeat1(choice(' ', '\n')),
+        member: $ => seq(
+            $.expr,
+            'in',
+            $.expr,
+        ),
+        guard: $ => seq(
+            'when',
+            $._ws,
+            $.expr,
+        ),
+        comprehension_block: $ => choice($.member, $.guard),
         comprehension: $ => seq(
             '{',
-            
-            
+            // optional($._ws),
+            'todo_val',
+            // optional($._ws),
+            '|',
+            // optional($._ws),
+            $.comprehension_block,
+            // optional($._ws),
+            // repeat(seq(optional($._ws), ',', optional($._ws), $.comprehension_block)),
+            // optional($._ws),
             '}',
         ),
     },
